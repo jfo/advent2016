e998a595982f2c02bd67f1fe4af3ece83d0493b4
Jeff Fowler <jeffowler@gmail.com>
2016-12-01 23:31:03 -0500

    rust problem one works

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -1,10 +1,11 @@
 use std::io::prelude::*;
 use std::fs::File;
 
-static NORTH : &'static [i32; 2] = &[0,  1];
-static EAST  : &'static [i32; 2] = &[1,  0];
-static SOUTH : &'static [i32; 2] = &[0, -1];
-static WEST  : &'static [i32; 2] = &[-1, 0];
+const NORTH : &'static [i32; 2] = &[0,  1];
+const EAST  : &'static [i32; 2] = &[1,  0];
+const SOUTH : &'static [i32; 2] = &[0, -1];
+const WEST  : &'static [i32; 2] = &[-1, 0];
+static CARDINALS  : [&'static[i32; 2]; 4] = [NORTH, EAST, SOUTH, WEST];
 
 fn travl(dir: &[i32; 2], steps: i32) -> (i32, i32) {
     (dir[0] * steps, dir[1] * steps)
@@ -14,26 +15,31 @@ fn main() {
     let mut f = File::open("../inputs/1.txt").unwrap();
     let mut s = String::new();
 
-    f.read_to_string(&mut s);
+    f.read_to_string(&mut s).unwrap();
     let len = s.len();
     s.truncate(len - 1);
+    let tokens = s.split(", ");
 
-    let thing: Vec<_> = [1,2,3].iter().zip([4,5,6].iter()).map(|e| e.0 + e.1).collect();
-    println!("{:?}", travl(NORTH, 4));
+    let directions = tokens.clone().scan(0, {|orientation, e|
+        match e.chars().next().unwrap() {
+            'L' => if *orientation == 0 {
+                *orientation += 3;
+                Some(CARDINALS[*orientation])
+            } else {
+                *orientation -= 1;
+                Some(CARDINALS[*orientation])
+            },
+            'R' => {
+                *orientation = (*orientation + 1) % 4;
+                Some(CARDINALS[*orientation])
+            }
+             _  => None,
+        }
+    });
 
-    // for e in s.split(", ") {
-    //     println!("{:?}", e);
-    // }
+    let movements = tokens.clone().map(|e| e[1..].parse::<i32>().unwrap());
+    let nodes = directions.zip(movements).map(|e| travl(e.0,e.1));
+    let distance_traveled = nodes.fold(0, |acc, e| acc + e.0 + e.1);
+
+    println!("{}", distance_traveled)
 }
-
-// if an instruction is modelled like, say, go north for 5 blocks:
-// [[0, 1], 5]
-// you could write a function that maps over the direction by multiplying by the distance
-// [0, 5]
-//
-// do this to a collection of single instructions and then reduce the collection like:
-// coll.push [0,0] for accumulator
-// coll.reduce {|acc, el| acc[0] += el[0]; acc[1] += el[1] }
-// or however more compactly that can be written
-//
-// you'll end up with the final position which can be summed for the answer.
