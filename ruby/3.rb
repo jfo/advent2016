39d47959f040fcfadeb53e55ad1c8edc3a1dd310
Jeff Fowler <jeffowler@gmail.com>
2016-12-03 01:44:50 -0500

    slicing differently

--- ruby/3.rb
+++ ruby/3.rb
@@ -1,8 +1,6 @@
 @input = File.open('./inputs/3.txt', "r").readlines.each {|l| l.chomp!}.collect! {|e| e.split(' ').map! {|e|e.to_i}}
-puts @input.select {|e| (e[0] + e[1] > e[2]) && (e[1] + e[2] > e[0]) && (e[2] + e[0] > e[1]) }.count
+is_valid = ->(e){(e[0] + e[1] > e[2]) && (e[1] + e[2] > e[0]) && (e[2] + e[0] > e[1])}
+slicediff = @input.map {|e| e[0]}.each_slice(3).to_a + @input.map {|e| e[1]}.each_slice(3).to_a + @input.map {|e| e[2]}.each_slice(3).to_a
 
-thing = @input.map {|e| e[0]}.each_slice(3).to_a +
-@input.map {|e| e[1]}.each_slice(3).to_a +
-@input.map {|e| e[2]}.each_slice(3).to_a
-
-puts thing.select {|e| (e[0] + e[1] > e[2]) && (e[1] + e[2] > e[0]) && (e[2] + e[0] > e[1]) }.count
+puts @input.select(&is_valid).count
+puts slicediff.select(&is_valid).count
