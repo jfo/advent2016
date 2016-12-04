9d5daf4a788323149910c6bb7c273d99d7b67be0
Jeff Fowler <jeffowler@gmail.com>
2016-12-04 00:53:34 -0500

    4.2

--- ruby/4.rb
+++ ruby/4.rb
@@ -1,20 +1,22 @@
+require 'rotn'
+
 @input = File.open('./inputs/4.txt', "r")
     .readlines.map {|e| e
         .chomp
-        .scan(/(^.+)\-(\d+)\[(.+)\]/)
+        .scan(/(^.+\-)(\d+)\[(.+)\]/)
         .flatten
-        .map {|e| e.split('').delete_if {|e| e == '-'}}
+        .map {|e| e.split('') }
     }
     .map {|e|
         [
-            e[0].sort_by {|x| [-e[0].count(x), x] }.uniq[0..4].join,
+            e[0].delete_if{|e| e == '-'}.sort_by {|x| [-e[0].count(x), x] }.uniq[0..4].join,
             e[1].join.to_i,
-            e[2].join
+            e[2].join,
+            e[0].join
         ]
     }
     .select {|e| e[0] == e[2]}
-    .map {|e| e[1]}
-    .inject :+
 
-p @input
 
+p @input .map {|e| e[1]} .inject :+
+p @input .map {|e| [e[3].rotn_shift({a:e[1] % 26}), e[1]]}.select {|e| e[0].include?("pole")}
