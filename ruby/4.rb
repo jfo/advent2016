15708174f1d61798bad2ed7dd4827f59427fd1af
Jeff Fowler <jeffowler@gmail.com>
2016-12-04 11:55:11 -0500

    making it smaller

--- ruby/4.rb
+++ ruby/4.rb
@@ -2,24 +2,18 @@ require 'rotn'
 
 @input = File.open('./inputs/4.txt', "r")
     .readlines.map {|e| e
-        .chomp
         .scan(/(^.+)\-(\d+)\[(.+)\]/)
         .flatten
     }
-    .map {|e|
-        [
-            e[0].split('')
-                .delete_if{|e| e == '-'}
-                .sort_by {|x| [-e[0].count(x), x] }
-                .uniq[0..4]
-                .join,
-            e[1].to_i,
-            e[2],
-            e[0]
-        ]
+    .select {|e| e[2] == e[0].split('')
+                         .delete_if{|e| e == '-'}
+                         .sort_by {|x| [-e[0].count(x), x] }
+                         .uniq[0..4]
+                         .join
     }
-    .select {|e| e[0] == e[2]}
 
-
-p @input .map {|e| e[1]} .inject :+
-p @input .map {|e| [e[3].rotn_shift({a:e[1] % 26}), e[1]]}.select {|e| e[0].include?("pole")}
+p @input .map { |e| e[1].to_i} .inject :+
+p @input .map { |e| [
+    e[0].rotn_shift({a:e[1].to_i % 26}),
+    e[1]
+]}.select { |e| e[0].include?("pole") }.first
