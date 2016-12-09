6d44cc157e08f68f98c23d04cef556e81636f9b4
Jeff Fowler <jeffowler@gmail.com>
2016-12-09 11:23:41 -0500

    count refactor

--- c/9.c
+++ c/9.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int count(FILE* fp) {
+    int num = 0;
+    char c;
+    while ((c = getc(fp)) != EOF )
+        num++;
+    return num;
+}
+
 int main() {
     FILE *fp = fopen("../inputs/9.txt", "r");
     if (!fp) { exit (1); }
 
-    char c;
-    while ((c = getc(fp)) != EOF )
-        printf("%c", c);
+    printf("%i", count(fp));
 }
