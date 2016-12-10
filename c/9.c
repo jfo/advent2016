3d3014d21c9acfdf0ab32ecbcd9fc8fee1d01933
Jeff Fowler <jeffowler@gmail.com>
2016-12-09 19:01:17 -0500

    comment

--- c/9.c
+++ c/9.c
@@ -79,7 +79,7 @@ int main() {
     /* char *test = "(27x12)(20x12)(13x14)(7x10)(1x12)A"; */
     /* char *test = "(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN"; */
 
-    printf("%li\n", decompressed_length(test, strlen(test)));
+    /* printf("%li\n", decompressed_length(test, strlen(test))); */
 
 
     free(input);
